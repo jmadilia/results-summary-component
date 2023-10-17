bfce3f3460a79840fd7619db725500f1ae7d95c3
Justin Madilia <jmadilia96@gmail.com>
2023-10-17 08:50:21 -0400

    Deploy initial solution.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,27 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        // Primary
+        'light-red': '#ff5757',
+        'orangey-yellow': '#ffb01f',
+        'green-teal': '#00bd91',
+        'cobalt-blue': '#1125d4',
+
+        // Gradients
+        'light-slate-blue': '#7857ff', // background
+        'light-royal-blue': '#2e2be9', // background
+        'violet-blue': 'var(--color-violet-blue)', // circle
+        'persian-blue': 'var(--color-persian-blue)', // circle
+
+        // Neutral
+        'pale-blue': '#ebf1ff',
+        'light-lavender': '#c8c7ff',
+        'dark-gray-blue': '#303b5a'
+      },
+      fontFamily: {
+        'hanken-grotesk': ['var(--font-hanken-grotesk)'],
+      }
     },
   },
   plugins: [],
